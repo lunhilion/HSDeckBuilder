93d106df03dfea38c2aed68122200fcf9f49c616
lunhilion <e.sanguin.92@gmail.com>
2016-11-29 16:10:10 +0100

    fix connessione al db

--- dbconnect.php
+++ dbconnect.php
@@ -2,10 +2,11 @@
 	$server_db = "localhost";
 	$user_db = "root";
 	$pwd_db = "";
+	$sel_db = "hs_db";
 
-	$conn_db = mysqli_connect($server_db,$user_db,$pwd_db);
+	$conn_db = mysqli_connect($server_db,$user_db,$pwd_db,$sel_db);
 
 	if(!$conn_db) {
-		die "Connessione fallita" . mysqli_query;
+		die "Connessione fallita" . mysqli_connect_error();
 	}
 ?>
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-  include("db_connect.php")
+  include("dbconnect.php")
 ?>
 <!DOCTYPE html>
 <html lang="en">
