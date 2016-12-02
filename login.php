9fbe56a7a64bca264b035f870a6552d48b8e3a91
lunhilion <e.sanguin.92@gmail.com>
2016-12-02 17:22:18 +0100

    mod login

--- login.php
+++ login.php
@@ -1,5 +1,15 @@
 <?php
-  include("dbconnect.php")
+  include("dbconnect.php");
+  session_start();
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $userLog = $_POST['userLog'];
+    $pwdLog = $_POST['pwdLog'];
+
+    $query = "SELECT username,password FROM users WHERE username='".$userLog."'AND password='".$pwdLog."';";
+    $rows = mysqli_query($conn_db, $query);
+    $numRows = mysqli_num_rows($rows);
+  }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,9 +29,12 @@
   			</div>
   		</div>
   	</nav>
+
   	<div class="container">
+
   		<div class="col-md-offset-4 col-md-4">
-  			<form method="POST" action="#">
+        <h1 class="text-center"> Login </h1>
+  			<form class="form-signin" method="POST" action="#">
 	  			<div class="form-group">
 	  				<label for="userLog">Utente</label>
 	  				<input type="text" class="form-control" placeholder="Inserisci qui il nome utente" name="userLog"></input>
@@ -32,9 +45,26 @@
   				</div>
   				<div class="btn-group-vertical btn-block">
   					<button type="submit" class="btn btn-primary">Accedi</button>
-  					<a href="registerForm.php" class="btn btn-warning">Registrati</a>
+  					<a href="register.php" class="btn btn-warning">Registrati</a>
   				</div>
   			</form>
+                <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            if(!empty($numRows)){
+                echo '
+                  <p><div class="alert alert-success text-center">
+                    Account <strong>'.$userLog.'</strong> presente!
+                  </div></p>';
+            } 
+            else {
+              echo '
+                <p><div class="alert alert-danger text-center">
+                    Account <strong>'.$userLog.'</strong> non presente!
+                  </div></p>
+                  ';
+            }
+        }
+      ?>
   	  </div>
     </div>
   </body>
